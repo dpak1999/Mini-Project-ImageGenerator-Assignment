452d7e5dc96fb646708c9e83eaf45b987fdb1f96
dpak1999 <deepakkumardash313@gmail.com>
2020-12-06 21:03:54 +0530

    add : img disp component & fetching img from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./css/App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageDisplay from "./Components/ImageDisplay";
+import { Button } from "react-bootstrap";
+import Loader from "./Components/Loader";
 
 const App = () => {
+  const [image, setImage] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchImage = () => {
+    fetch("https://picsum.photos/500").then(
+      (result) => {
+        setLoading(true);
+        setImage(result.url);
+      },
+      (err) => {
+        setLoading(true);
+        setError(err);
+      }
+    );
+  };
+
+  const getImage = (e) => {
+    e.preventDefault();
+    fetchImage();
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
   return (
     <div className="App">
-      App
-      <ImageDisplay />
-      <button>Get Image</button>
+      {error ? (
+        <h3>Failed to fetch</h3>
+      ) : !loading ? (
+        <Loader />
+      ) : (
+        <ImageDisplay image={image} />
+      )}
+      <br />
+      <Button variant="outline-primary" className="mt-2" onClick={getImage}>
+        Get Image
+      </Button>
     </div>
   );
 };
--- src/Components/ImageDisplay.js
+++ src/Components/ImageDisplay.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Loader from "./Loader";
 
-const ImageDisplay = () => {
-  return <div>Image display</div>;
+const ImageDisplay = (props) => {
+  return (
+    <div>
+      <h1>Here is your Image</h1>
+      {!props.image ? <Loader /> : <img src={props.image} />}
+    </div>
+  );
 };
 
 export default ImageDisplay;
